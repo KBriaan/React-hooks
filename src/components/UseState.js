5891a028daa08df2926f22c93e3766cde2eabde9
K Briaan <kbriaan1@gmail.com>
2024-07-22 12:56:23 +0300

    second commit

--- src/components/UseState.js
+++ src/components/UseState.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const UseState = () => {
+  const [counter, setCounter]=useState(0)
+  const increment =()=>{
+    setCounter(3)
+  }
   return (
     <div>
-      usestate
+      {counter}<button onClick={increment}>Increment</button>
     </div>
   )
 }
